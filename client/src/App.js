2ca49d442a906d6b50c27fef8afeda976d89dbf6
RL2020 <loc.fitness1@gmail.com>
2022-07-11 12:48:44 -0400

    first half of layout, some content

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <body>  
+      <ul>
+      
+        <li><a href="news.asp">Search</a></li>
+        <li><a href="contact.asp">List of Medications</a></li>
+        <li><a href="about.asp">Shopping Cart</a></li>
+      </ul>
+    <div class="container-1">      
+    <h1> Online Pharmacy</h1>
+    <p> Welcome to the online Pharmacy!</p>
+    <p>Where you can browse and purchase your medications online </p>     
+    <p>from the comfort of your own home</p> 
     </div>
+
+    <div class="container-1-list" >   
+    {/* <!-- <ul> --> */}
+        <li>Get your medication safe and secure</li>
+        <li> Order ships same day order is placed</li>
+        <li> Great customer service 24/7 </li>
+    
+    {/* <!-- </ul> --></div> */}
+    {/* <!-- <div class="image-container-1"> --> */}
+        <img class="middle-container-pic" src="images/olga-delawrence-b9CaYHNfmho-unsplash.jpg" alt="pill bottles" /> 
+    </div>
+    </body>
   );
 }
 
