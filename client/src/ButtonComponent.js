3141a990f73360e683721c153b4c215e423e0bba
RL2020 <loc.fitness1@gmail.com>
2022-08-24 15:15:30 -0400

    testing button function

--- client/src/ButtonComponent.js
+++ client/src/ButtonComponent.js
@@ -1,12 +1,25 @@
-import React from "react";
+import * as React from "react";
 
 //signup 
 //log in
 //browes drugs
 
 
+
 function ButtonComponent() {
-    return <button> Click Me !</button>
+    const [count, setCount] = React.useState(0)
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+    return( 
+        <div>     
+    <button type= "button" onclick={handleClick}>
+         Click Me !
+    </button>
+    {count}
+        </div>
+    
+    );
 }
 
 
